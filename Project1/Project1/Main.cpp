b3ab54e9f58f84e7306d207c0aadb82c66301031
Minh Thang, Nguyen <nguyenminhthang013@gmail.com>
2025-11-19 16:25:50 -0800

    Add files via upload

---
+++ Project1/Project1/Main.cpp
@@ -0,0 +1,22 @@
+/*
+	Nguyen, Minh Thang
+
+    Fall 2025
+    CS A250 - C++ 2
+
+    Bot Elections
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    cout << "Hi\n";
+
+    cout << endl;
+	system("Pause");
+    return 0;
+}
