8e7b5482f26ff38455f4e0b7fab70b22ee72bba0
Minh Thang, Nguyen <nguyenminhthang013@gmail.com>
2025-11-19 16:14:16 -0800

    Create BotType class for bot elections
    Define BotType class with attributes and methods.

---
+++ Project1/BotType.h
@@ -0,0 +1,38 @@
+/*
+	Nguyen, Minh Thang 
+
+	Fall 2025
+	CS A250 - C++ 2
+
+	Bot Elections
+*/
+
+#ifndef  BOTTYPE_H
+#define BOTTYPE_H
+
+#include <string>
+
+class BotType
+{
+public:
+	BotType() : name(), ability(), trait() {}
+	BotType(const std::string& botName, const std::string& botAbility,
+		const std::string& botTrait)
+		: name(botName), ability(botAbility), trait(botTrait) {}
+	
+	std::string getBotName() const;
+	std::string getBotAbility() const;
+	std::string getBotTrait() const;
+
+	void printBotName() const;
+	void printBotInfo() const;
+	
+	bool operator<(const BotType&) const;
+
+private:
+	std::string name;
+	std::string ability;
+	std::string trait;
+};
+
+#endif BOTTYPE_H
